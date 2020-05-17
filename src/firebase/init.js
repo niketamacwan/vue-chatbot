e66ce1ad1bd26e9b01035e8289cd93d994d82c6f
niketamacwan <niketamacwan816@gmail.com>
2020-05-18 00:44:40 +0530

    firestore issue resolved

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,16 +1,16 @@
 import firebase from "firebase/app";
-//import firestore from "firebase/firestore";
+import firestore from "firebase/firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAc9t-N30owy2iAHGq9HqhX2GDTtR8hhBM",
-  authDomain: "vuechatapp-82314.firebaseapp.com",
-  databaseURL: "https://vuechatapp-82314.firebaseio.com",
-  projectId: "vuechatapp-82314",
-  storageBucket: "vuechatapp-82314.appspot.com",
-  messagingSenderId: "302389795875",
-  appId: "1:302389795875:web:cf61695553fb65e3a72596",
-  measurementId: "G-GDFJNE351L"
+    apiKey: "AIzaSyAc9t-N30owy2iAHGq9HqhX2GDTtR8hhBM",
+    authDomain: "vuechatapp-82314.firebaseapp.com",
+    databaseURL: "https://vuechatapp-82314.firebaseio.com",
+    projectId: "vuechatapp-82314",
+    storageBucket: "vuechatapp-82314.appspot.com",
+    messagingSenderId: "302389795875",
+    appId: "1:302389795875:web:cf61695553fb65e3a72596",
+    measurementId: "G-GDFJNE351L"
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
