8251ea6f9075d5e839ddffc4b03ef5b81b767734
niketamacwan <niketamacwan816@gmail.com>
2020-05-17 23:19:39 +0530

    Changed single qoutes and build the app

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import firestore from 'firebase/firestore';
+import firebase from "firebase/app";
+//import firestore from "firebase/firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
